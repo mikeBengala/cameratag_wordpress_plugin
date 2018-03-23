6ebd0c205ea75640e0755e2b27ef8d8b9eca58aa
ninjaMike <miguelbengala@hotmail.com>
2018-03-23 18:12:11 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="pt">
+<head>
+    <?php $app_id = "a-73f30cb0-10e6-0136-88fd-0adeb7a592e0";?>
+    <script src='//cameratag.com/api/v12/js/cameratag.min.js' type='text/javascript'></script>
+    <link rel='stylesheet' href='//cameratag.com/static/12/cameratag.css'></link>
+<meta charset="utf-8">
+<title></title>
+</head>
+
+<body>
+    <camera data-app-id='<?=$app_id?>' id='myCamera'></camera>
+</body>
+</html>
